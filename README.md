bf0818a0e799dc0ce8f79f9d9d0b335baf34cdf0
Sebastian Buck <sebastian.s.buck@daimler.com>
2020-01-05 17:53:11 +0100

    Add webview that shows ldd, nm and file information

---
+++ README.md
@@ -0,0 +1,17 @@
+# Linux Binary Preview
+
+[![Licence](https://img.shields.io/github/license/betwo/vscode-linux-binary-preview.svg)](https://github.com/betwo/vscode-linux-binary-preview)
+[![VS Code Marketplace](https://vsmarketplacebadge.apphb.com/version-short/betwo.vscode-linux-binary-preview.svg) ![Rating](https://vsmarketplacebadge.apphb.com/rating-short/betwo.vscode-linux-binary-preview.svg) ![Downloads](https://vsmarketplacebadge.apphb.com/downloads-short/betwo.vscode-linux-binary-preview.svg) ![Installs](https://vsmarketplacebadge.apphb.com/installs-short/betwo.vscode-linux-binary-preview.svg)](https://marketplace.visualstudio.com/items?itemName=betwo.vscode-linux-binary-preview)
+
+![Preview GIF](https://raw.githubusercontent.com/betwo/vscode-linux-binary-preview/master/assets/preview.gif)
+
+Open binary Linux files and display information about the symbols contained.
+
+Supported file types are:
+ * Shared objects (.so)
+ * Archives (.a)
+
+Displayed information is the output of the commands
+ * `file` (File information)
+ * `ldd` (Linked libraries, if available)
+ * `nm` (Contained symbols)
---
+++ src/extension.ts
@@ -0,0 +1,66 @@
+import * as vscode from 'vscode';
+
+import { SharedObjectContentProvider } from './content_provider_so';
+
+const so_provider = new SharedObjectContentProvider();
+
+export async function openTextDocument(document: vscode.TextDocument, context: vscode.ExtensionContext) {
+  if (document === undefined) {
+    if (vscode.window.activeTextEditor !== undefined) {
+      document = vscode.window.activeTextEditor.document;
+    }
+  }
+  if (document === undefined) {
+    vscode.window.showWarningMessage("Cannot show binary information, no document is opened.");
+    return;
+  }
+  if (document.languageId === 'shared_object' || document.languageId === 'archive') {
+    openBinaryFilePreview(document.uri, context);
+  }
+}
+
+export async function openBinaryFilePreview(uri: vscode.Uri, context: vscode.ExtensionContext) {
+  let panel = vscode.window.createWebviewPanel(
+    'binary viewer',
+    uri.fsPath.toString(),
+    vscode.ViewColumn.Active,
+    {
+      enableScripts: true,
+      enableFindWidget: true
+    }
+  );
+
+  panel.webview.onDidReceiveMessage(
+    message => {
+      switch (message.command) {
+        case 'showBinary':
+          openBinaryFilePreview(vscode.Uri.parse(message.text), context);
+          return;
+      }
+    },
+    undefined,
+    context.subscriptions
+  );
+
+  panel.webview.html = await so_provider.toHTML(uri);
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  const openCommand = vscode.commands.registerCommand(
+    'linux-binary-preview.openCurrentFile', (thisArg: any, ...args: any[]) => {
+      openTextDocument(args[0], context);
+    });
+
+  const openEvent = vscode.workspace.onDidOpenTextDocument(
+    (document: vscode.TextDocument) => {
+      openTextDocument(document, context);
+    });
+
+  for (let document of vscode.workspace.textDocuments) {
+    openTextDocument(document, context);
+  }
+
+  context.subscriptions.push(openCommand, openEvent);
+}
+
+export function deactivate() { }
